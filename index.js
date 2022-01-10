47117e8f6d02637a3ecfd924c1380b753e442050
chuongvu2001 <chuongvu2806@gmail.com>
2022-01-10 23:32:01 +0700

    test morgan

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
+app.use(morgan('combined'))
 app.get('/', (req, res) => {
-    var a = 1;
-    var b = 2;
-    var c = a + b;
-    return res.send(console.log(c));
+    return res.send('hello world!');
 })
 
 app.listen(port, () => {
